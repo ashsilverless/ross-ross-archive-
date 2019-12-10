e93624b2809b993a6d4478b37f68fcbfd1a4b1b4
Rob Clark <rob@silverless.co.uk>
2019-12-10 11:27:32 +0000

    Hero Build

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -7,17 +7,28 @@ endif;?>
 
 <div class="hero <?php the_field('hero_height');?>" style="background-image: url(<?php echo $heroImage['url']; ?>); background-color: <?php echo $heroColor; ?>;">
 
-    <div class="container cols-3-12-6-3">
-        <div class="col"></div>
+    <div class="container">
 		<div class="col">
 		    <div class="hero__content">
                 <div class="inner-section">
-                    <h1 class="heading heading__md heading__light mt0 mb0"></h1>
+                	<div class="company-title">
+	                	<img src="https://via.placeholder.com/300x150">
+	                    <h1 class="heading heading__md heading__light mt0 mb0">
+	                    	<?php the_field('hero_heading');?>
+	                    </h1>
+	                    <h2>
+	                    	<?php the_field('hero_subheading');?>
+	                    </h2>
+	                </div>
+	                <div class="company-copy">
+	                    <h3>
+	                    	<?php the_field('hero_copy');?>
+	                    </h3>
+	                </div>
+	                <div id="scroll-icon">
+	                	<span></span>
+	                </div>
                 </div>
-				<!--<div class="buttons">
-					<a href=""  class="">Button Text</a>
-					<a href="">Button Text</a>
-				</div>-->
 		    </div>
 		</div>
 	</div>
